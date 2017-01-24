ca4e39f2f6b4002ddc3945ffc2c04575beb8e268
Kent Watsen <kent@watsen.net>
2017-01-24 09:47:03 -0500

    fixing YANG to use rc:ayng-data

--- refs/gen-trees.sh
+++ refs/gen-trees.sh
@@ -1,2 +1,7 @@
-pyang -f tree ../ietf-voucher\@*.yang > ietf-voucher-tree.txt 
-pyang -f tree ../ietf-voucher-revocation\@*.yang > ietf-voucher-revocation-tree.txt 
+#pyang -f tree ../ietf-voucher\@*.yang > ietf-voucher-tree.txt 
+#pyang -f tree ../ietf-voucher-revocation\@*.yang > ietf-voucher-revocation-tree.txt 
+
+pyang -p ../ -f tree --tree-print-groupings ../ietf-voucher\@*.yang | sed -e '2,3 d' -e 's/^    //' > ietf-voucher-tree.txt 
+
+pyang -p ../ -f tree --tree-print-groupings ../ietf-voucher-revocation\@*.yang | sed -e '2,3 d' -e 's/^    //' > ietf-voucher-revocation-tree.txt
+
