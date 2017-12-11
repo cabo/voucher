d086f22aaee242b1d60fc5065ee6748915e99a8d
Michael Richardson <mcr@sandelman.ca>
2017-12-11 09:57:17 -0500

    putting origin yang files in subdir fails because pyang searches subdirectories, so put generated files into subdir

--- refs/gen-trees.sh
+++ refs/gen-trees.sh
@@ -1,6 +1,6 @@
-#pyang -f tree ../ietf-voucher\@*.yang > ietf-voucher-tree.txt 
-#pyang -f tree ../ietf-voucher-revocation\@*.yang > ietf-voucher-revocation-tree.txt 
+#pyang -f tree ../ietf-voucher\@*.yang > ietf-voucher-tree.txt
+#pyang -f tree ../ietf-voucher-revocation\@*.yang > ietf-voucher-revocation-tree.txt
 
-pyang -p ../ -f tree --tree-print-yang-data ../ietf-voucher\@*.yang > ietf-voucher-tree.txt 
+pyang -p ../ -f tree --tree-print-yang-data ../yang/ietf-voucher\@*.yang > ietf-voucher-tree.txt
 
 
